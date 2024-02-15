2a003758772ea72aca7a7ae4cb44d8844a92dde8
Roberto-Larivera <robylariv@gmail.com>
2024-02-15 23:17:25 +0100

    first commit

---
+++ lib/widgets/grocery_list.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_list/data/dummy_items.dart';
+import 'package:shopping_list/widgets/new_item.dart';
+
+class GroceryList extends StatefulWidget {
+  const GroceryList({super.key});
+  @override
+  State<StatefulWidget> createState() {
+    return _GroceryListState();
+  }
+}
+
+class _GroceryListState extends State<GroceryList> {
+  void _addNewItem() {
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (ctx) => const NewItem(),
+    ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Groceries'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: _addNewItem,
+          ),
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: groceryItems.length,
+        itemBuilder: (ctx, index) => ListTile(
+          title: Text(groceryItems[index].name),
+          leading: Container(
+            width: 24,
+            height: 24,
+            color: groceryItems[index].category.color,
+          ),
+          trailing: Text('${groceryItems[index].quantity}'),
+        ),
+      ),
+    );
+  }
+}
