766e759c4635e3645e05496c3a6b615a5ade346a
Roberto-Larivera <robylariv@gmail.com>
2024-02-15 23:59:27 +0100

    Aggiornamenti al modulo NewItem

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+// import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/categories.dart';
 
@@ -11,6 +11,11 @@ class NewItem extends StatefulWidget {
 }
 
 class _NewItemState extends State<NewItem> {
+  final _formKey = GlobalKey<FormState>();
+
+  void _submitForm(){
+    _formKey.currentState!.validate();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,28 +25,45 @@ class _NewItemState extends State<NewItem> {
         body: Padding(
           padding: const EdgeInsets.all(12),
           child: Form(
+            key: _formKey,
             child: Column(
               children: [
                 TextFormField(
+                  keyboardType: TextInputType.text,
                   maxLength: 50,
                   decoration: const InputDecoration(
                       // labelText: 'Name',
                       label: Text('Name')),
-                  // validator: (value){
-                  //   if(value){
-                  //     return 'Please enter a name';
-                  //   }
-                  //   return null;
-                  // },
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter a name';
+                    } else if (value.trim().length <= 2) {
+                      return 'Name must be at least 2 characters long';
+                    } else if (value.trim().length > 50) {
+                      return 'Name must be less than 50 characters long';
+                    }
+                    return null;
+                  },
                 ),
                 Row(
                   crossAxisAlignment: CrossAxisAlignment.end,
                   children: [
                     Expanded(
                       child: TextFormField(
+                        keyboardType: TextInputType.number,
                         decoration:
                             const InputDecoration(label: Text('Quantity')),
                         initialValue: '1',
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter a quantity';
+                          } else if (int.tryParse(value) == null) {
+                            return 'Quantity must be a number';
+                          } else if (int.tryParse(value)! <= 0) {
+                            return 'Quantity must be greater than 0';
+                          } 
+                          return null;
+                        },
                       ),
                     ),
                     const SizedBox(width: 8),
@@ -64,6 +86,20 @@ class _NewItemState extends State<NewItem> {
                           )
                       ], onChanged: (value) {}),
                     ),
+                  ],
+                ),
+                const SizedBox(height: 12),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        _formKey.currentState!.reset();
+                      },
+                      child: const Text('Reset'),
+                    ),
+                    ElevatedButton(
+                        onPressed: _submitForm, child: const Text('Add Item'))
                   ],
                 )
               ],
